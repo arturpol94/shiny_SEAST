5e6dbb945f9e799e6ef70c33715a256de5d56315
arturpol94 <banach.artur@mail.com>
2016-05-19 19:01:35 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -1,5 +1,5 @@
 
-
+library(ggplot2)
 library(shiny)
 library(dplyr)
 library(lubridate)
@@ -8,56 +8,59 @@ library(lubridate)
 
 shinyServer(function(input, output) {
     
-    ##-------------------------------------------------------------------------
+  
+  tweets_frame <- reactive({
+    in_file <- input$in_file  # Accedemos al input
     
-    ##TAREA2: Rellenar para que tweets_frame sea reactivo
+    if (is.null(in_file))
+      return(NULL)
     
-    ##-------------------------------------------------------------------------
+    file_path <- in_file$datapath 
     
-    ## Tabla de tweets
+    read.table(file_path, header = TRUE,sep = "\t", stringsAsFactors = FALSE) %>%
+      
+      mutate(.,
+             anno = year(fecha),
+             mes = month(fecha),
+             dia = day(fecha),
+             hora = hour(fecha),
+             minuto = minute(fecha),
+             hora0 = (dia - min(dia)) * 24 + hora + minuto/60
+      )
     
-    ##---------------------------------------------------------------------
+  })
+   
     
-    ##TAREA2: Modificar para que trabaje con expresión reactiva
-    
-    ##---------------------------------------------------------------------
     
     output$tweets <- renderTable({
         
-        in_file <- input$in_file  # Accedemos al input
-        
-        if (is.null(in_file))
-            return(NULL)
-        
-        file_path <- in_file$datapath  # in_file es una lista. El path esta en datapath.
-        
-        tweets_frame <- read.table(file_path, header = TRUE,
-                                   sep = "\t", stringsAsFactors = FALSE) %>%
-            mutate(.,
-                   anno = year(fecha),
-                   mes = month(fecha),
-                   dia = day(fecha),
-                   hora = hour(fecha),
-                   minuto = minute(fecha),
-                   hora0 = (dia - min(dia)) * 24 + hora + minuto/60
-            )
-        
+       
         ##---------------------------------------------------------------------
         
         ##TAREA1: Cambiar para que el numero de filas lo elija el usuario.
         
         ##---------------------------------------------------------------------
         
-        return(head(tweets_frame, 5))
+        return(head(tweets_frame(), input$display))
 
     })
     
-    ##-------------------------------------------------------------------------
+   
     
-    ##TAREA2: Implementar grafico de evolucion de tweets
-    
-    ##-------------------------------------------------------------------------
-    
+    output$plot <- renderPlot({
+      
+      copia <- tweets_frame()
+      copia <- filter(copia, fecha>input$range[1] & fecha<input$range[2])
+      
+      
+      tabla <- copia 
+      
+      tabla$hora0 <- cut(tabla$hora0, input$slider)
+      
+      ggplot(data=tabla, aes(x=hora0, color = animo, fill=animo)) +
+        geom_bar(position = "dodge")
+      
+    })
 })
 
 
